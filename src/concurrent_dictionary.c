60ee38fc9a54a2e67afc5a3b82254325f646d78f
Ivan Ushakov <ivan.ushakov@gmail.com>
2015-04-22 16:50:11 +0300

    put operation must follow SPMC

--- src/concurrent_dictionary.c
+++ src/concurrent_dictionary.c
@@ -10,9 +10,10 @@
 
 struct concurrent_dictionary_ {
     ck_ht_t table;
+    pthread_mutex_t mutex;
 };
 
-static void *ht_malloc(size_t r)
+static void *ht_allocate(size_t r)
 {
     return memory_allocator_new(r);
 }
@@ -23,39 +24,46 @@ static void ht_free(void *p, size_t b, bool r)
 }
 
 static struct ck_malloc my_allocator = {
-    .malloc = ht_malloc,
+    .malloc = ht_allocate,
     .free = ht_free
 };
 
 concurrent_dictionary *concurrent_dictionary_create()
 {
-    concurrent_dictionary *dictionary = malloc(sizeof(concurrent_dictionary));
-    if (dictionary == NULL)
+    concurrent_dictionary *result = malloc(sizeof(concurrent_dictionary));
+    if (result == NULL)
     {
         error("concurrent_dictionary_create:: create");
         return NULL;
     }
-    memset(dictionary, 0, sizeof(concurrent_dictionary));
+    memset(result, 0, sizeof(concurrent_dictionary));
 
     unsigned int mode = CK_HT_MODE_BYTESTRING;
-    if (ck_ht_init(&(dictionary->table), mode, NULL, &my_allocator, 2, 6602834) == false)
+    if (ck_ht_init(&(result->table), mode, NULL, &my_allocator, 2, 6602834) == false)
     {
         error("concurrent_dictionary_create:: hash table");
         return NULL;
     }
 
-    return dictionary;
+    if (pthread_mutex_init(&(result->mutex), NULL))
+    {
+        perror("concurrent_dictionary_create:: mutex");
+        return NULL;
+    }
+
+    return result;
 }
 
 void concurrent_dictionary_release(concurrent_dictionary *dictionary)
 {
+    pthread_mutex_destroy(&(dictionary->mutex));
     free(dictionary);
 }
 
 void *concurrent_dictionary_find(concurrent_dictionary *dictionary, const char *key)
 {
     ck_ht_hash_t h;
-    size_t key_length = strlen(key);
+    uint16_t key_length = (uint16_t)strlen(key);
     ck_ht_hash(&h, &(dictionary->table), key, key_length);
 
     ck_ht_entry_t entry;
@@ -68,12 +76,22 @@ void *concurrent_dictionary_find(concurrent_dictionary *dictionary, const char *
 
 int concurrent_dictionary_add(concurrent_dictionary *dictionary, const char *key, void *value)
 {
-    size_t key_length = strlen(key);
+    if (pthread_mutex_lock(&(dictionary->mutex)))
+    {
+        error("concurrent_dictionary_add:: mutex lock");
+        return 0;
+    }
+
+    uint16_t key_length = (uint16_t)strlen(key);
     ck_ht_hash_t h;
     ck_ht_hash(&h, &(dictionary->table), key, key_length);
 
     ck_ht_entry_t entry;
     ck_ht_entry_set(&entry, h, key, key_length, value);
 
-    return ck_ht_put_spmc(&(dictionary->table), h, &entry);
+    bool result = ck_ht_put_spmc(&(dictionary->table), h, &entry);
+
+    pthread_mutex_unlock(&(dictionary->mutex));
+
+    return result;
 }
